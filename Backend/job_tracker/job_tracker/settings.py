78d08ccec90869cfca817ae94ceed6586464d9ba
Alfredocgn <alfredocgn@gmail.com>
2024-07-05 15:23:20 -0300

    frontend started

--- Backend/job_tracker/job_tracker/settings.py
+++ Backend/job_tracker/job_tracker/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    'http://localhost:5173',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import './App.css'
+import { Login } from './components/Login'
+
+function App() {
+
+
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path='/login' Component={Login}/>
+      </Routes>
+    </Router>
+    
+    </>
+  )
+}
+
+export default App
